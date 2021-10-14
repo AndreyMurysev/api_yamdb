40a7cce816af919846ebe77bf7024603fb319fac
Alexey Rudnev <work.rudnev@yandex.ru>
2021-10-14 16:31:12 +0300

    Models

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,8 +1,8 @@
-from django_filters import filters
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets, mixins, filters
 
 from reviews.models import Category, Genre, Title
+from .filters import TitlesFilter
 from .serializers import UserSerializer, CategorySerializer, GenreSerializer, TitleSerializer
 from users.models import User
 
@@ -12,35 +12,36 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
 
-class ListCreateDestroyViewSet(
-    mixins.ListModelMixin,
-    mixins.CreateModelMixin,
+class DestroyCreateListViewSet(
     mixins.DestroyModelMixin,
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
     viewsets.GenericViewSet,
 ):
     pass
 
 
-class CategoryViewSet(ListCreateDestroyViewSet):
+class CategoryViewSet(DestroyCreateListViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
-    filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
 
 
-class GenreViewSet(ListCreateDestroyViewSet):
+class GenreViewSet(DestroyCreateListViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
-    filter_backends = (filters.SearchFilter,)
     search_fields = ('name',)
     lookup_field = 'slug'
+    filter_backends = (filters.SearchFilter,)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
     queryset = Title.objects.all()
     serializer_class = TitleSerializer
-    filter_backends = [DjangoFilterBackend]
     filterset_class = TitlesFilter
+    filter_backends = [DjangoFilterBackend]
+
 
 
