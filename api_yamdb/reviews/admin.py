6fd4e6e330496a1d7c6652a5bb7c47d975878bdb
Alexey Rudnev <work.rudnev@yandex.ru>
2021-10-20 21:52:47 +0300

    reviews/admin

--- api_yamdb/reviews/admin.py
+++ api_yamdb/reviews/admin.py
@@ -1,36 +1,30 @@
 from django.contrib import admin
 from django.conf import settings
 
-from reviews.models import Category, Genre, Title
+from .models import Category, Genre, Title
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'name',
-        'slug'
-    )
+    list_display = ('id',
+                    'name',
+                    'slug')
     empty_value_display = settings.VOID
 
 
 @admin.register(Genre)
 class GenreAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'name',
-        'slug'
-    )
+    list_display = ('id',
+                    'name',
+                    'slug')
     empty_value_display = settings.VOID
 
 
 @admin.register(Title)
 class TitleAdmin(admin.ModelAdmin):
-    list_display = (
-        'id',
-        'name',
-        'year',
-        'description',
-        'category'
-    )
+    list_display = ('id',
+                    'name',
+                    'year',
+                    'description',
+                    'category')
     empty_value_display = settings.VOID
