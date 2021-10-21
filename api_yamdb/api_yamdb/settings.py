da1cc66eb10f117dbca84c34b260d571d4a7ec41
Андрей М <andreimurysev@yandex.ru>
2021-10-21 12:44:27 +0300

    settinngs/HOSTS

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -15,7 +15,7 @@ SECRET_KEY = env('SECRET_KEY', default=None)
 
 DEBUG = True
 
-ALLOWED_HOSTS: list[str] = env('ALLOWED_HOSTS', default='["*"]')
+ALLOWED_HOSTS = env('ALLOWED_HOSTS', default='["*"]')
 
 AUTH_USER_MODEL = 'users.User'
 
